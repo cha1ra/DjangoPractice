720b2c20386fe04b45a654038c9f03a9ac6f9157
cha1ra <s11397as@cha1ra.com>
2018-09-27 11:31:49 +0900

    スタティックフォルダとの連携・フォームの作成(w/o using Formクラス)

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -4,8 +4,17 @@ from django.http import HttpResponse
 def index(request):
     params = {
         'title':'Hello/Index',
-        'msg':'これは、サンプルで作ったページです。',
+        'msg':'お名前は？',
         'goto':'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def form(request):
+    msg = request.POST['msg']
+    params = {
+        'title':'Hello/Form',
+        'msg':'こんにちは、' + msg + 'さん。',
+        'goto':'next'
     }
     return render(request, 'hello/index.html', params)
 
