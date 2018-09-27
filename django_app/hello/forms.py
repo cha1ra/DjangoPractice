5d9b2944aa0b5421a53b6d4c3a67900cdca96e7e
cha1ra <s11397as@cha1ra.com>
2018-09-28 02:53:30 +0900

    選択リスト作成

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -4,6 +4,9 @@ class HelloForm(forms.Form):
     data = [
         ('one', 'item 1'),
         ('two', 'item 2'),
-        ('three', 'item 3')
+        ('three', 'item 3'),
+        ('four', 'item 4'),
+        ('five', 'item 5')
     ]
-    choice = forms.ChoiceField(label='choice', choices=data)
+    choice = forms.ChoiceField(label='radio',\
+         choices=data, widget=forms.Select(attrs={'size':5}))
