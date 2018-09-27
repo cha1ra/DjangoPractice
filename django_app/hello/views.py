6255ee4dde86d723e7e36c596ce98cd2bcd69a14
cha1ra <s11397as@cha1ra.com>
2018-09-27 10:39:58 +0900

    templateを用いてビューを作成する

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request, age, nickname):
-    result = 'your account: ' + nickname + '" (' + str(age) + ').'
-    return HttpResponse(result)
-    
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'これは、サンプルで作ったページです。',
+        'goto':'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'これは、もう一つのページです',
+        'goto':'index',
+    }
+    return render(request, 'hello/index.html', params)
